90534c6c11578af47e356fabb033060fd1e123b1
Suryadeep Pal <suryadeep10@gmail.com>
2016-05-26 19:32:21 +0530

    clock.py updated for heroku deplyement

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ import threading
 def scrap_call():
 	while True:
 		try:
-			a = requests.post('http://localhost:8000/scrap', timeout=40)
+			a = requests.post('http://skfginotifier.herokuapp.com/scrap', timeout=40)
 			print(a.json())
 		except requests.exceptions.ReadTimeout as e:
 			print(e)
